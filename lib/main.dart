2e0c771d4e3424ac55f0edb02d3fddf5c2e4f2de
vyom1102 <95407663+vyom1102@users.noreply.github.com>
2024-02-24 17:05:31 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:clima/screens/loading_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main()  =>runApp( MyApp());
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
+
